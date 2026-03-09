abb7a0edd8a54b7a045d3ffd22ec09b1bc04f9d2
divineuzoma3159 <divineu647@gmail.com>
2026-03-09 18:46:10 +0100

    Refactor comments in script.js for clarity
    Updated comments for clarity and consistency throughout the script.

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
-const openUp = document.querySelector('#openUp');
-const myList = document.querySelector('#myList');
+const openUp = document.querySelector('#openUp');//the div holding the menu bar
+const myList = document.querySelector('#myList');//the remaining list shown whan the menu bar is clicked
 openUp.addEventListener('click',() => {
-    myList.classList.toggle('hide');
+    myList.classList.toggle('hide');//for when the menu bar shows and the phone width is less than 480px
 });
-const myTips = document.querySelector('#myTips');
-function moveIn1(){
+const myTips = document.querySelector('#myTips');//the tips section
+function moveIn1(){//to have a smoth scroll animation when moving to the tips section
     myTips.scrollIntoView({behavior:'smooth'});
     myTips.style.animation = `flooring ${1}s ease-in-out`;
 };
-const muscleGroup = document.querySelector('#muscleGroup');
-function moveIn2(){
+const muscleGroup = document.querySelector('#muscleGroup');//the muscle group section
+function moveIn2(){//to have a smoth scroll animation when moving to the muscle group section
     muscleGroup.scrollIntoView({behavior:'smooth'});
     muscleGroup.style.animation = `flooring ${1}s ease-in-out`;
 };
-const footer = document.querySelector('footer');
-function moveIn3(){
+const footer = document.querySelector('footer');//The footer
+function moveIn3(){//to have a smoth scroll animation when moving to the footer
     footer.scrollIntoView({behavior:'smooth'});
     footer.style.animation =  `flooring ${1}s ease-in-out`;
 };
-const lastInit = document.querySelector('#lastInit');
-function joinNow(){
+const lastInit = document.querySelector('#lastInit');//the form section
+function joinNow(){//to have a smoth scroll animation when moving to the form section
     lastInit.scrollIntoView({behavior:'smooth'});
     lastInit.style.animation =  `flooring ${1}s ease-in-out`;
 };
-const myForm = document.querySelector('form');
-myForm.addEventListener('submit',e => {
+const myForm = document.querySelector('form');//the form
+myForm.addEventListener('submit',e => {//to prevent the form from refreshing
     e.preventDefault();
 })
-const myCancel = document.querySelector('#myCancel');
-const myName = document.querySelector('#myName');
-const myEmail = document.querySelector('#myEmail');
-const myWeight = document.querySelector('#myWeight');
-const myHeight = document.querySelector('#myHeight');
-myCancel.addEventListener('click', () => {
+const myCancel = document.querySelector('#myCancel');//the submit button in the form
+myCancel.addEventListener('click', () => {//to reset the form and give an alert once the submit button has been clicked
     alert('your info has been recieved');
     myForm.reset();
 });
-const scrolls = document.querySelectorAll('.scroll');
-function scrolling(){
+const scrolls = document.querySelectorAll('.scroll');//the sections
+function scrolling(){//scroll animation for each section
     scrolls.forEach(into => {
         const inner = into.getBoundingClientRect();
         if(inner.top < window.innerHeight - 100){
@@ -46,4 +42,5 @@ function scrolling(){
     });
 }
 window.addEventListener("scroll",scrolling);
+
 scrolling();
